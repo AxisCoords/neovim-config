0d7a48ea70629dd1b5940e6f42d313977de5e4ff
Axis <49685684+AxisCoords@users.noreply.github.com>
2026-07-17 14:02:43 +0200

    Renabled line length limit

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.clipboard = "unnamedplus"
---vim.opt.colorcolumn = "100"
+vim.opt.colorcolumn = "100"
 
 -- UI
 vim.opt.number = true
